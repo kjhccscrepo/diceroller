43d3d9977cee8c014acd56e5e7b9c7e9f06b2e9f
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-02-04 14:50:31 -0600

    Update DiceRoller.cpp

--- DiceRoller.cpp
+++ DiceRoller.cpp
@@ -8,36 +8,39 @@ int MaximumRoll;
 double AverageRoll;
 double IntNumFix;
 double TestVar;
+int remainingrolls;
+int rollcount = 1;
 
 int main()
 {
     srand(time(0));
-
     std::cout << "Welcome to the Dice Roller Program\n\n";
     std::cout << "How many dice do you want to roll: ";
     std::cin >> HowManyDice;
-    std::cout << "\n\nHow many sides are on these dice: ";
+    std::cout << "\nHow many sides are on these dice: ";
     std::cin >> HowManySides;
 
     std::cout << "\n\nWhen you roll your " << HowManyDice << "d" << HowManySides << ", the stats are...\n";
     MaximumRoll = (HowManyDice * HowManySides);
     IntNumFix = MaximumRoll;
-    AverageRoll = MaximumRoll/2;
+    AverageRoll = MaximumRoll / 2;
     IntNumFix = HowManyDice;
-    AverageRoll = (AverageRoll += (IntNumFix/2));
+    AverageRoll = (AverageRoll += (IntNumFix / 2));
 
 
-    std::cout << "\nMinimum: " << HowManyDice; 
+    std::cout << "\nMinimum: " << HowManyDice;
     std::cout << "\nAverage: " << AverageRoll;
     std::cout << "\nMaximum: " << MaximumRoll;
+    std::cout << "\n";
 
-    do {
+    for (remainingrolls = HowManyDice; remainingrolls > 0; rollcount++) {
 
-        CurrentDiceRoll = (rand() % 6 + 1);
-        std::cout << "\n" << CurrentDiceRoll;
-      
+        CurrentDiceRoll = (rand() % HowManySides + 1);
+        std::cout << "\nRoll #" << rollcount << " was " << CurrentDiceRoll;
+        TotalDiceRoll = TotalDiceRoll + CurrentDiceRoll;
+        remainingrolls -= 1;
 
-    
-    } while (0);
+    }
+    std::cout << "\n\nThe total of the " << HowManyDice << " rolls was " << TotalDiceRoll << ".";
     return 0;
 }
